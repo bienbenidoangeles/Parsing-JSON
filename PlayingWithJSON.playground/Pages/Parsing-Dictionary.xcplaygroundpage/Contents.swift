1b557fc932f24e6d7a129da4c04a2130b4ae091a
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-07-28 12:24:34 -0400

    parsed contacts dictionary

--- PlayingWithJSON.playground/Pages/Parsing-Dictionary.xcplaygroundpage/Contents.swift
+++ PlayingWithJSON.playground/Pages/Parsing-Dictionary.xcplaygroundpage/Contents.swift
@@ -1,3 +1,48 @@
 //Parsing Dictionary
 
+import Foundation
 
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+
+//=========================================================
+//Create Model(s)
+//=========================================================
+
+//Codable: Decodable & Encodable
+
+//Decodable: converst JSON Data
+//Encodable: converts to json data to e.g. POST to a web API
+
+struct ResultsWrapper: Codable {
+    let results: [Contact]
+}
+
+struct Contact: Codable {
+    let firstName: String
+    let lastName: String
+}
+
+//=======================================================
+// DECODE the JSON Data to our swift model
+//=======================================================
+
+do {
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results
+    dump(contacts)
+} catch {
+    print("decoding error: \(error)")
+}
